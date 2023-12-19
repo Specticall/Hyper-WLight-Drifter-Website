89726601f479d4a004de3bb383f6fc50f57e08da
Specticall <josephyusmita@gmail.com>
2023-12-20 01:15:39 +0700

    fixed some bugs with the map slide

--- js/map.js
+++ js/map.js
@@ -1,5 +1,12 @@
 const mapEl = document.querySelector(".boss .container");
 const mapDragEl = document.querySelector(".boss .container .lower-map");
+const skullIcon = document.querySelectorAll(".boss-pin .skull");
+const popupIcon = document.querySelectorAll(".boss .popup");
+const dragMatEl = document.querySelector(".boss .drag-mat");
+
+const sectionEl = document.querySelector(".section.boss");
+
+let isMousedown = false;
 
 const setMapPosition = (targetPercentage) => {
   mapDragEl.style.width = `${targetPercentage}%`;
@@ -14,12 +21,46 @@ const handleMouseMove = (e) => {
   setMapPosition(movePercentage);
 };
 
-mapEl.addEventListener("mousedown", () => {
+const detachListener = () => {
+  isMousedown = false;
+  dragMatEl.style.display = "none";
+  mapEl.style.cursor = "grab";
+  mapEl.removeEventListener("mousemove", handleMouseMove);
+};
+
+sectionEl.addEventListener("mousedown", () => {
+  isMousedown = true;
+  dragMatEl.style.display = "block";
   mapEl.style.cursor = "grabbing";
   mapEl.addEventListener("mousemove", handleMouseMove);
 });
 
 mapEl.addEventListener("mouseup", () => {
-  mapEl.style.cursor = "grab";
-  mapEl.removeEventListener("mousemove", handleMouseMove);
+  detachListener();
 });
+
+dragMatEl.addEventListener("mouseleave", () => {
+  detachListener();
+});
+
+// skullIcon.forEach((skull) => {
+//   skull.addEventListener("mousedown", () => {
+//     dragMatEl.style.display = "block";
+//     mapEl.style.cursor = "grabbing";
+//     mapEl.addEventListener("mousemove", handleMouseMove);
+//   });
+
+//   // skull.addEventListener("mouseover", () => {
+//   //   dragMatEl.style.display = "none";
+//   //   mapEl.style.cursor = "grab";
+//   //   mapEl.removeEventListener("mousemove", handleMouseMove);
+//   // });
+// });
+
+// popupIcon.forEach((popup) => {
+//   popup.addEventListener("mousedown", () => {
+//     dragMatEl.style.display = "block";
+//     mapEl.style.cursor = "grabbing";
+//     mapEl.addEventListener("mousemove", handleMouseMove);
+//   });
+// });
